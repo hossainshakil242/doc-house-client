2226b33098b800a5950e692aa496f6aa8cf612ef
hossainshakil242 <hossainshakil242@gmail.com>
2023-10-14 16:11:34 +0600

    Navbar and Banner maintaince

---
+++ src/pages/home/banner/Banner.jsx
@@ -0,0 +1,23 @@
+import img1 from '../../../assets/home/banner/image1.png';
+import img2 from '../../../assets/home/banner/image2.png';
+import img3 from '../../../assets/home/banner/image3.png';
+const Banner = () => {
+    return (
+        <div className="bg-[#07332F] mx-w-[1600] mx-2 text-white md:p-48">
+            <div className='flex flex-col md:flex-row items-center gap-3'>
+                <div className='text-center md:text-left'>
+                    <h1 className='text-5xl font-semibold'>Your Best Medical<br />Help Center</h1>
+                    <p>Lorem Ipsum is simply dummy text they are printing typesetting <br/> has been the industry’s stardard.</p>
+                    <button className="btn btn-success">All Service</button>
+                </div>
+                <div className='relative p-28'>
+                    <img src={img1} className='absolute top-0 left-0' alt="" />
+                    <img src={img2} className='absolute top-1/2 left-3/5 ' alt="" />
+                    <img src={img3} className='absolute -top-10 -right-full ' alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/pages/home/home/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../../shared/navbar/Navbar";
+import Banner from "../banner/Banner";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
